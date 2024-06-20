31ec7fdc170e869ff9814667622a9da333811143
Stan van Dijk <stan.vandijk2001@outlook.com>
2024-06-20 18:31:20 +0200

    Setup working

---
+++ index.js
@@ -0,0 +1,222 @@
+"use strict";
+// #region TYPES/CLASSES
+class Buttons {
+    position;
+    color;
+    direction;
+    constructor(color, indexes, direction) {
+        this.position = new Vector2(indexes[0] + 0.5, indexes[1] + 0.5);
+        this.color = color;
+        this.direction = direction || Math.floor(Math.random() * 4);
+    }
+    updateDirection() {
+        switch (this.direction) {
+            case Direction.UP:
+                this.direction = Direction.RIGHT;
+                break;
+            case Direction.RIGHT:
+                this.direction = Direction.DOWN;
+                break;
+            case Direction.DOWN:
+                this.direction = Direction.LEFT;
+                break;
+            case Direction.LEFT:
+                this.direction = Direction.UP;
+                break;
+        }
+    }
+}
+class Vector2 {
+    x;
+    y;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    array() {
+        return [this.x, this.y];
+    }
+}
+var Direction;
+(function (Direction) {
+    Direction[Direction["UP"] = 0] = "UP";
+    Direction[Direction["RIGHT"] = 1] = "RIGHT";
+    Direction[Direction["DOWN"] = 2] = "DOWN";
+    Direction[Direction["LEFT"] = 3] = "LEFT";
+})(Direction || (Direction = {}));
+var Mode;
+(function (Mode) {
+    Mode[Mode["SET"] = 0] = "SET";
+    Mode[Mode["PLAY"] = 1] = "PLAY";
+})(Mode || (Mode = {}));
+// #endregion
+// #region UTILS_PROPS
+const CUBE_SIZE = 3;
+const GRID_ROWS = CUBE_SIZE;
+const GRID_COLS = CUBE_SIZE;
+const SPACE = 32;
+const buttons = [];
+let mode = Mode.SET;
+// #endregion
+// #region MAIN
+(() => {
+    const game = document.getElementById("game");
+    if (!game)
+        throw new Error("Game not found");
+    game.width = 800;
+    game.height = 800;
+    const ctx = game.getContext("2d");
+    if (!ctx)
+        throw new Error("Canvas not found");
+    window.addEventListener("keypress", (event) => keyDownEvent(event, ctx));
+    window.addEventListener("mousedown", (event) => mouseDownEvent(event, ctx));
+    ctx.fillStyle = "#181818";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.scale(ctx.canvas.width / GRID_COLS, ctx.canvas.height / GRID_ROWS);
+    ctx.strokeStyle = "#303030";
+    ctx.lineWidth = 0.05;
+    for (let x = 0; x <= GRID_COLS; ++x) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, GRID_ROWS);
+        ctx.stroke();
+    }
+    for (let y = 0; y <= GRID_ROWS; ++y) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(GRID_COLS, y);
+        ctx.stroke();
+    }
+    for (let x = 0; x < GRID_COLS; ++x) {
+        for (let y = 0; y < GRID_ROWS; ++y) {
+            buttons.push(new Buttons("#202020", [x, y]));
+        }
+    }
+    renderButtons(ctx);
+})();
+// #endregion
+// #region EVENTS
+function keyDownEvent(event, ctx) {
+    switch (event.key.charCodeAt(0)) {
+        case SPACE:
+            const current = mode;
+            mode = mode === Mode.SET ? Mode.PLAY : Mode.SET;
+            console.log("Switched mode from", Mode[current], "to", Mode[mode]);
+            break;
+        default:
+            console.log("Key pressed:", event.key);
+    }
+    switch (mode) {
+        case Mode.PLAY:
+            console.log("Play mode");
+            gameLogic(ctx);
+            break;
+        default:
+            break;
+    }
+}
+function mouseDownEvent(event, ctx) {
+    const x_index = Math.floor(event.offsetX / (ctx.canvas.width / GRID_COLS));
+    const y_index = Math.floor(event.offsetY / (ctx.canvas.height / GRID_ROWS));
+    const button = getButton(x_index, y_index);
+    if (!button) {
+        // Outside the grid
+        return;
+    }
+    switch (mode) {
+        case Mode.SET:
+            button.updateDirection();
+            break;
+        case Mode.PLAY:
+            updateButtons(x_index, y_index);
+            break;
+        default:
+            break;
+    }
+    renderButtons(ctx);
+}
+// #endregion
+// #region RENDER
+function renderButtonText(ctx, button) {
+    function renderArrowStrokes(ctx, first, middle, last) {
+        ctx.moveTo(...first.array());
+        ctx.lineTo(...middle.array());
+        ctx.lineTo(...last.array());
+    }
+    const move_pixels = 0.20;
+    const center = new Vector2(button.position.x, button.position.y);
+    const top = new Vector2(center.x, center.y - move_pixels);
+    const left = new Vector2(center.x - move_pixels, center.y);
+    const right = new Vector2(center.x + move_pixels, center.y);
+    const bottom = new Vector2(center.x, center.y + move_pixels);
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.lineWidth = 0.05;
+    ctx.beginPath();
+    switch (button.direction) {
+        case Direction.UP:
+            renderArrowStrokes(ctx, left, top, right);
+            break;
+        case Direction.RIGHT:
+            renderArrowStrokes(ctx, top, right, bottom);
+            break;
+        case Direction.DOWN:
+            renderArrowStrokes(ctx, right, bottom, left);
+            break;
+        case Direction.LEFT:
+            renderArrowStrokes(ctx, bottom, left, top);
+            break;
+    }
+    ctx.stroke();
+}
+function circle(ctx, button) {
+    // Circle with the color
+    ctx.fillStyle = button.color;
+    ctx.beginPath();
+    ctx.arc(...button.position.array(), 0.45, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = "#FFFFFF";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = "0.3px sans-serif";
+    renderButtonText(ctx, button);
+}
+function renderButtons(ctx) {
+    buttons.forEach((button) => circle(ctx, button));
+}
+// #endregion
+// #region GAME
+function gameLogic(ctx) {
+    if (allOneDirection()) {
+        console.log("All buttons are in the same direction");
+    }
+}
+// #endregion
+// #region UTILS
+function getButton(x, y) {
+    if (!inBounds(x, y)) {
+        return undefined;
+    }
+    return buttons[y + x * GRID_COLS];
+}
+function inBounds(x, y) {
+    return x >= 0 && x < GRID_COLS && y >= 0 && y < GRID_ROWS;
+}
+function allOneDirection() {
+    return buttons.every((button) => button.direction === Direction.UP) ||
+        buttons.every((button) => button.direction === Direction.RIGHT) ||
+        buttons.every((button) => button.direction === Direction.DOWN) ||
+        buttons.every((button) => button.direction === Direction.LEFT);
+}
+function updateButtons(x, y) {
+    for (let i = -1; i <= 1; ++i) {
+        for (let j = -1; j <= 1; ++j) {
+            const next = getButton(x + j, y + i);
+            if (!next) {
+                // No button found
+                continue;
+            }
+            next.updateDirection();
+        }
+    }
+}
+// #endregion
